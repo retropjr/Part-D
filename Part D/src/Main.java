93740ff876acc04a1550dc878c2b126c26435a8c
Richard Porter <retropjr@hotmail.com>
2020-12-22 15:25:16 +1300

    First commit.  With first line of output.

---
+++ Part D/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		//Display four lines of text.  Commit to the local repository and push to
+		//GitHub after each line of code is added.
+		System.out.println("Hi, my name is Oscar");
+
+	}
+
+}
