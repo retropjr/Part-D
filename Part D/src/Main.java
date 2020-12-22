2b0414db3b32dfaaf6229f3cf420cf6126ae6278
Richard Porter <retropjr@hotmail.com>
2020-12-22 15:27:58 +1300

    Line 2

--- Part D/src/Main.java
+++ Part D/src/Main.java
@@ -4,7 +4,10 @@ public class Main {
 	public static void main(String[] args) {
 		//Display four lines of text.  Commit to the local repository and push to
 		//GitHub after each line of code is added.
+		//1
 		System.out.println("Hi, my name is Oscar");
+		//2
+		System.out.println("My favourite colour is Green");
 
 	}
 
